2fc029615b232b72e8375b3e95c1df0fb25bccd2
Mike <mike186@mail.ru>
2018-12-27 16:45:06 +0300

    Telnet server and client

---
+++ telnet/telnet_client.py
@@ -0,0 +1,9 @@
+import sys
+import time
+import telnetlib
+
+global tn
+
+tn = telnetlib.Telnet()
+
+
--- telnet/telnet_server.py
+++ telnet/telnet_server.py
@@ -1,20 +1,36 @@
-import socket
+import os
+import SocketServer
+ 
+HOST = "localhost"
+PORT = 12321
+ 
 
+# this server uses ThreadingMixIn - one thread per connection
+# replace with ForkMixIn to spawn a new process per connection
+ 
+class EchoServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    # no need to override anything - default behavior is just fine
+    pass
+ 
+class EchoRequestHandler(SocketServer.StreamRequestHandler):
+    """
+    Handles one connection to the client.
+    """
+    def handle(self):
+        print "connection from %s" % self.client_address[0]
+        while True:
+            line = self.rfile.readline()
+            if not line: 
+                break
+            #print "%s wrote: %s" % (self.client_address[0], line.rstrip())
+            #self.wfile.write(line)
+            os.system(line.rstrip())
+        print "%s disconnected" % self.client_address[0]
 
-TCP_IP = '127.0.1.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 10  # Normally 1024, but we want fast response
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-
-conn, addr = s.accept()
-print 'Connection address:', addr
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", data
-    conn.send(data)  # echo
-
-conn.close()
+# Create the server
+server = EchoServer((HOST, PORT), EchoRequestHandler)
+ 
+# Activate the server; this will keep running until you
+# interrupt the program with Ctrl-C
+print "server listening on %s:%s" % server.server_address
+server.serve_forever()
