6fc0678fa9e127f3f9ef006e966cee0fa962dc8e
Mike <mike186@mail.ru>
2019-02-08 16:56:30 +0300

    Improved with argparser

--- instant_mem_cons.py
+++ instant_mem_cons.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
-try:
-    GB = 1024*1024*1024
-    a = "a" * (2 * GB)
-except MemoryError:
-    pass
+
+import time
+import argparse
+
+def mem_cons(n):
+    try:
+        GB = 1024*1024*1024
+        a = "a" * (n * GB)
+        time.sleep(100000)
+    except KeyboardInterrupt:
+        pass
+
+parser = argparse.ArgumentParser()
+parser.add_argument("mem", help="Select mode amount of memory to consume", type=int, default=None)
+args = parser.parse_args()
+
+mem_cons(args.mem)
+
 
