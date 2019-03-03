7db9740ba0de2cdbd71a8d8b50b8765a59c8e904
Unknown <mike186@mail.ru>
2019-03-03 20:43:52 +0300

    Made staggered RAM eat

--- instant_mem_cons.py
+++ instant_mem_cons.py
@@ -2,19 +2,28 @@
 
 import time
 import argparse
+from multiprocessing import Manager, Pool, Process, cpu_count
+eat = 30
 
 def mem_cons(n):
-    try:
-        GB = 1024*1024*1024
-        a = "a" * (n * GB)
-        time.sleep(100000)
-    except KeyboardInterrupt:
-        pass
+        print(n)
+        list=[]
+        while n > 1:
+            GB = 1024*1024*1024
+            a = "a" * (n * GB)
+            time.sleep(10)
+            list.append(a)
+            print("Some memory was eaten")
+            n = n/2 
+            print(n)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("mem", help="Select mode amount of memory to consume", type=int, default=None)
-args = parser.parse_args()
+        print("It's time for a sleep for 10 seconds")
+        time.sleep(10)
 
-mem_cons(args.mem)
+def multiproc(func):
+    p1 = Process(target=mem_cons, args=(func,))
+    p1.start()
+    p1.join()
 
+multiproc(eat)
 
